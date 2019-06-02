37159f25bdefd02b08720ca02550ba7bd1dbee78
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-06-01 20:51:25 -0400

    Done

--- README.md
+++ README.md
@@ -17,54 +17,54 @@ Siri-like Homework Assignment
     >Liri
         +keys.js
             -This file holds API access information in order for us to connect to one of our API's
-        +index.js
-            -This file ......
+        +liri.js
+            -This file includes all logic to Liri.
         +package.json
             -Entry point for API communication
 
 #Screenshots/Videos
 
-    >Screenshots
-
-
-
     >Videos
-
+        Google Drive:
+            https://drive.google.com/file/d/1vUkfaavpOqYSh87O7wG8ZyVTDO51an2_/view?usp=sharing
 
 # How to Use Liri
-
+Syntax for all functions start with `node liri `
 Look up a Song
+    `spotify-this-song <song name here>`
 
-    Step 1 -
+    Step 1 - Call the Spotify case function with `spotify-this-song`
 
-    Step 2 - 
+    Step 2 - Put a space after.
 
-    Step 3 - 
+    Step 3 - <song name here> you can type the name of the song inside of the "<>" without the "<>"
 
-    Step 4 - 
+    Step 4 - Hit Enter
 
-    Step 5 -
+    Step 5 - Look for your results in the log.txt file
 
 Look up a Concert
+    `concert-this <artist/band name here>`
 
-    Step 1 -
+    Step 1 - Call the Band is in Town case function with `concert-this`
 
-    Step 2 - 
+    Step 2 - Put a space after.
 
-    Step 3 - 
+    Step 3 - <artist/band name here> you can type the name of the artist/band inside of the "<>" without the "<>"
 
-    Step 4 - 
+    Step 4 - Hit Enter
 
-    Step 5 -
+    Step 5 - Look for your results in the log.txt file
 
 Look up a Movie
+    `movie-this '<movie name here>'`
 
-    Step 1 -
+    Step 1 - Call the OMDB case function with `movie-this`
 
-    Step 2 - 
+    Step 2 - Put a space after.
 
-    Step 3 - 
+    Step 3 - <movie name here> you can type the name of the movie inside of the "<>" without the "<>"
 
-    Step 4 - 
+    Step 4 - Hit Enter
 
-    Step 5 -
+    Step 5 - Look for your results in the log.txt file
--- liri.js
+++ liri.js
@@ -1,22 +1,93 @@
 
-var keys = require("./keys.js");
-require("./.env");
+require("dotenv").config();
+const keys = require("./keys.js");
+const fs = require("fs");
+const axios = require("axios");
+const moment = require("moment");
 
+let userInput = process.argv.slice(2, 3).join("");
+let userSearch = process.argv.slice(3).join(" ");
 
-
-
-
-var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify({
-    id: "235e29ce984841838b53fd8d781f7905",
-    secret: "267b3ab016ef429a9d15c2c9760bfa77"
-});
-
-spotify.search({ type: 'track', query: 'I want it that way' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
+//The switch statement to pick the correct function
+function siri() {
+    switch (userInput) {
+        case "concert-this":
+            concert();
+            break;
+        case "spotify-this-song":
+            spotify();
+            break;
+        case "movie-this":
+            movie();
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+        default:
+            break;
     }
+};
 
-    console.log(data);
-});
+siri();
+//Functions
+
+function spotify() {
+    //use keys variable to get spotify id and secret from keys.js
+    var Spotify = require('node-spotify-api');
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: userSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        fs.appendFile("log.txt", data.tracks.items[0].name + "\n", function (err) { });
+        fs.appendFile("log.txt", data.tracks.items[0].preview_url + "\n", function (err) { });
+        fs.appendFile("log.txt", data.tracks.items[0].album.artists[0].name + "\n", function (err) { });
+        fs.appendFile("log.txt", data.tracks.items[0].album.name + "\n \n", function (err) { });
+    });
+
+}
+
+function movie() {
+    if (userSearch === "") {
+        userSearch = "Mr. Nobody";
+        console.log(movie);
+    }
+    axios.get("http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy").then(function (request) {
+
+        fs.appendFile("log.txt", request.data.Title + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Year + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Rated + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Ratings[1] + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Country + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Language + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Plot + "\n", function (err) { });
+        fs.appendFile("log.txt", request.data.Actors + "\n \n", function (err) { });
+
+
+    })
+
+}
+
+function concert() {
+    axios.get("https://rest.bandsintown.com/artists/"
+        + userSearch + "/events?app_id=codingbootcamp").then(function (request) {
+            fs.appendFile("log.txt", request.data[0].venue.name + "\n", function (err) { });
+            fs.appendFile("log.txt", request.data[0].venue.city + "\n", function (err) { });
+            fs.appendFile("log.txt", request.data[0].venue.region + "\n \n", function (err) { });
+            fs.appendFile("log.txt", moment(request.data[0].datetime).format('MMMM Do YYYY, h:mm a') + "\nBe there or be square!", function (err) { });
+
+        });
+}
+
+function doIt() {
+
+    fs.readFile("./random.txt", function (err, data) {
+        let dataString = data.toString();
+        userInput = dataString.split(" ").slice(0, 1).join("");
+        userSearch = dataString.split(" ").slice(1).join(" ");
+        siri();
+        fs.appendFile("log.txt", data + "\n \n", function (err) { });
+    });
+}
